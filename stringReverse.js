a010e04473acf8ed6fa1b78f4f91fc65861ae5c7
Manmath kapale <mkapale96@gmail.com>
2024-03-13 22:23:36 +0530

    feat: string reverse

---
+++ stringReverse.js
@@ -0,0 +1,21 @@
+/**
+ * Reverses a given string.
+ *
+ * @param {string} str - The input string to be reversed.
+ * @return {string} The reversed string.
+ */
+function reverseString(str) {
+  if (!str) {
+    throw new Error("Input string is null or undefined.");
+  }
+
+  let reversedString = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedString += str[i];
+  }
+
+  return reversedString;
+}
+
+console.log(reverse(""));
