ba1b3fe110c4a246b00709bb932438241e710a56
Manmath kapale <mkapale96@gmail.com>
2024-03-13 22:31:43 +0530

    fix: updates

--- code/stringReverse.js
+++ code/stringReverse.js
@@ -8,13 +8,11 @@ function reverseString(str) {
   if (!str) {
     throw new Error("Input string is null or undefined.");
   }
-
   let reversedString = "";
 
   for (let i = str.length - 1; i >= 0; i--) {
     reversedString += str[i];
   }
-
   return reversedString;
 }
 
