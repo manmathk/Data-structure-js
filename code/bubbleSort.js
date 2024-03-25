6df051392e156e8e83455e0d4eda26921bf596e1
Manmath kapale <mkapale96@gmail.com>
2024-03-25 21:20:00 +0530

    fix: updates

--- code/bubbleSort.js
+++ code/bubbleSort.js
@@ -2,7 +2,7 @@
 
 function bubbleSort(arr, n) {
   for (let i = 0; i < n - 1; i++) {
-    for (let j = 0; j < n; j++) {
+    for (let j = 0; j < n - i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j];
         arr[j] = arr[j + 1];
